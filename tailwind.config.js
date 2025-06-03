4ce62dfe0bda656d4b1d208ec2b8bc9667974a44
BARATHRAJ <jcibarathraj@gmail.com>
2025-06-03 21:20:10 +0530

    Removed Dark and Added 3D

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,9 +35,15 @@ export default {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-10px)' },
         },
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
       },
       animation: {
         float: 'float 3s ease-in-out infinite',
+        gradient: 'gradient 6s ease infinite',
       },
     },
   },
